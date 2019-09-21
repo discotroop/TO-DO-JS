0322ac63bef58f9b37995480c9ef67a54a24426c
John Mark Goodson <john.mark.hat@gmail.com>
2019-09-20 21:48:18 -0400

    mild editssigning off for the night.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 export function test() {
     console.log("test");
 }
+
+// console versions first.
+
+// needs to export and array of objects that make up the to do list.
+    // objects need data - corresponds to index
+    // 
+
+// needs factory function/constructor to build objects.
